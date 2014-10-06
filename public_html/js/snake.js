df4343e03a2f6d217a8f5e29d21f00bad3e087b3
Jack Spinuzza <Explosion1199@gmail.com>
2014-10-06 10:44:06 -0700

    Second Commit; again not very intersting still new files and fixes site not yet operational.

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,3 +1,25 @@
+var snake;
 
+var context;
+var screenWidth;
+var screenHight;
 
+function gameInitalize(){
+    var canvas = documnet.getElementById("game-screen")
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    context.fillstyle = rgb
+}
 
